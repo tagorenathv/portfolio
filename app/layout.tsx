1119cc17cdf0358f82646ed55f1d5ca2377f8e41
TagorenathV <tagorenath.v@gmail.com>
2024-06-01 23:57:07 +0200

    updated profile pic

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,8 @@ export const metadata = {
     images: [
       {
         url: "https://www.tagorenathv.me/profile-pic.png",
-        width: 800,
-        height: 600,
+        width: 500,
+        height: 500,
       },
     ],
     description: "I am Tagorenath V, a seasoned software engineer specializing in designing scalable and cost-efficient software architectures. Dive into my portfolio to see the technologies I work with, detailed case studies of projects I've led, and the key achievements throughout my career. Additionally, I am committed to contributing to open-source projects and supporting startups in their digital transformation efforts, leveraging my expertise to foster innovation and growth.",
@@ -38,9 +38,9 @@ export const metadata = {
     creator: 'Tagorenath V',
     images: [
       {
-        url: "https://www.tagorenathv.me/profile-pic.png",
-        width: 800,
-        height: 600,
+        url: "https://www.tagorenathv.me/profile.png",
+        width: 500,
+        height: 500,
       },
     ],
   },
